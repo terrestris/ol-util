d976699a6059459887dd7a2312075b94d01e8779
Marc Jansen <jansen@terrestris.de>
2018-09-11 17:47:00 +0200

    Use constant MULTI_GEOM_PREFIX

--- src/GeometryUtil/GeometryUtil.js
+++ src/GeometryUtil/GeometryUtil.js
@@ -170,10 +170,11 @@ class GeometryUtil {
    * @returns {ol.geom.Multipoint|ol.geom.MultiPolygon|ol.geom.MultiLinestring} A Multigeometry.
    */
   static mergeGeometries(geometries) {
+    const multiPrefix = GeometryUtil.MULTI_GEOM_PREFIX;
     let geomType = geometries[0].getType();
     let mixedGeometryTypes = false;
     geometries.forEach(geometry => {
-      if (geomType.replace('Multi', '') !== geometry.getType().replace('Multi', '')) {
+      if (geomType.replace(multiPrefix, '') !== geometry.getType().replace(multiPrefix, '')) {
         mixedGeometryTypes = true;
       }
     });
@@ -184,8 +185,8 @@ class GeometryUtil {
 
     // split all multi-geometries to simple ones if passed geometries are
     // multigeometries
-    if (geomType.startsWith(GeometryUtil.MULTI_GEOM_PREFIX)) {
-      const multiGeomPartType = geomType.substring(GeometryUtil.MULTI_GEOM_PREFIX.length);
+    if (geomType.startsWith(multiPrefix)) {
+      const multiGeomPartType = geomType.substring(multiPrefix.length);
       geometries = GeometryUtil.separateGeometries(geometries);
       geomType = multiGeomPartType;
     }
