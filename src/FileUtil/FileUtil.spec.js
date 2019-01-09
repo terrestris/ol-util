bd1a5ae81f9cabdb166bb0ee9c69c245e11d25ba
Marc Jansen <jansen@terrestris.de>
2019-01-09 16:03:37 +0100

    Add a test for addShpLayerFromFile

--- src/FileUtil/FileUtil.spec.js
+++ src/FileUtil/FileUtil.spec.js
@@ -7,9 +7,33 @@ import {
   FileUtil
 } from '../index';
 
+import shpwrite from 'shp-write';
+
+
+const geoJson2 = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [47, -11]
+    },
+    properties: {
+      song: 'If you have ghosts'
+    }
+  }]
+};
+
+
 describe('FileUtil', () => {
   const geoJsonFile = new File([JSON.stringify(geoJson)], 'geo.json', {
     type: 'application/json',
+    lastModified: new Date()
+  });
+
+  const shpBuffer = shpwrite.zip(geoJson2);
+  const shpFile = new File([new Blob([shpBuffer])], 'geo.zip', {
+    type: 'application/zip',
     lastModified: new Date()
   });
 
@@ -56,6 +80,32 @@ describe('FileUtil', () => {
             resolve();
           });
           FileUtil.addGeojsonLayerFromFile(geoJsonFile, map);
+        });
+      });
+    });
+
+    describe('#addShpLayerFromFile', () => {
+      it('reads the shp file and adds a layer to the map', () => {
+        expect.assertions(6);
+        return new Promise((resolve) => {
+          const layers = map.getLayers();
+          layers.on('add', (event) => {
+            const layer = event.element;
+            expect(layers.getLength()).toBe(2);
+
+            var features = layer.getSource().getFeatures();
+            expect(features.length).toBe(1);
+            var feat = features[0];
+            var coords = feat.getGeometry().getCoordinates();
+            var properties = feat.getProperties();
+
+            expect(coords[0]).toBe(47);
+            expect(coords[1]).toBe(-11);
+            expect('song' in properties).toBe(true);
+            expect(properties.song).toBe('If you have ghosts');
+            resolve();
+          });
+          FileUtil.addShpLayerFromFile(shpFile, map);
         });
       });
     });
