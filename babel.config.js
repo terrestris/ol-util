0f0d7c7e52e8ec7ebdfa1ac4606e31cba33e0898
Daniel Koch <koch@terrestris.de>
2021-04-08 09:35:10 +0200

    Add @babel/plugin-transform-runtime and @babel/runtime

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
   'plugins': [
     '@babel/plugin-proposal-function-bind',
     '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime',
     ['@babel/plugin-proposal-class-properties', { 'loose': false }],
   ]
 };
