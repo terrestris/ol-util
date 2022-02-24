79bf16f3b05dc7faa489201aead90bf42e12ee64
Simon Seyock <seyock@terrestris.de>
2022-02-24 17:36:08 +0100

    chore: enable typescript to emit declarations

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   'presets': [
     '@babel/env',
-    '@babel/react'
+    '@babel/react',
+    '@babel/preset-typescript'
   ],
   'plugins': [
     '@babel/plugin-proposal-function-bind',
     '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-typescript',
     ['@babel/plugin-proposal-class-properties', { 'loose': false }],
   ]
 };
