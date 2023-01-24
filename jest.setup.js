380e07816c65646c4297c5621c5655a9750c3fb7
Daniel Koch <koch@terrestris.de>
2023-01-24 09:34:19 +0100

    chore: update packages (ol and geostyler-openlayers-parser) and remove unused ones

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 global.fetch = jest.fn();
+
+Object.defineProperty(global, 'ResizeObserver', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn()
+  }))
+});
